e94e05982c4635ce9b4f558946d6951c92b465b2
Victor Perdiguer <victor.perdiguer@gmail.com>
2022-12-10 11:48:07 +0100

    lab done

--- index.js
+++ index.js
@@ -1,17 +1,49 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [];
+    this.length = this.items.length;
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item);
+    this.length = this.items.length;
+    this.items.sort((a, b) => a - b);
+  }
 
-  get(pos) {}
+  get(pos) {
+    if (pos >= this.items.length) {
+      throw new Error('OutOfBounds');
+    }
+    return this.items[pos];
+  }
 
-  max() {}
+  max() {
+    this._error();
+    return this.items[this.length - 1];
+  }
 
-  min() {}
+  min() {
+    this._error();
+    return this.items[0];
+  }
 
-  sum() {}
+  sum() {
+    if (this.length === 0) {
+      return 0;
+    }
+    return this.items.reduce((sum, elem) => sum += elem);
+  }
 
-  avg() {}
+  avg() {
+    this._error();
+    return this.sum()/this.length;
+  }
+
+  _error() {
+    if (this.length === 0) {
+      throw new Error('EmptySortedList');
+    }
+  }
 }
 
 module.exports = SortedList;
